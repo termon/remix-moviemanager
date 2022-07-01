4917b8be4033e8479c238646f06e8d2b02121800
Aiden McCaughey <a.mccaughey@ulster.ac.uk>
2022-07-01 14:27:18 +0100

    Refactor SelectList to accept enumeration prop

--- app/components/Shared/EnumSelectList.tsx
+++ app/components/Shared/EnumSelectList.tsx
@@ -1,19 +1,18 @@
-import { Genre } from "~/lib/base"
 
 type PropType = {
-  type: any
+  enumeration: any
   name: string,
   defaultValue: any,
   className: string
 }
-export default function SelectList( {type, name, defaultValue, className}: PropType) {
+export default function EnumSelectList( {enumeration, name, defaultValue, className}: PropType) {
   return ( 
     <select name={name} defaultValue={defaultValue} className={className} >
       <option value="-1" disabled>Choose a Genre...</option>
-      {Object.keys(typeof(type))
+      {Object.keys(enumeration)
         .filter( f => !isNaN(Number(f)))
         .map(key => (
-        <option key={key} value={key}>{Genre[key as any]}</option>
+        <option key={key} value={key}>{enumeration[key as any]}</option>
       ))}
     </select>
   )  
